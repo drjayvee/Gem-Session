b6ebb1f747a76f614f5b85929501f7a7e1a32c0a
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-07-17 21:42:57 +0200

    Add project scaffold
    I wanted to call this resource GemSession, but that conflicted with the
    app's module name.

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_17_192415) do
+  create_table "projects", force: :cascade do |t|
+    t.text "prompt", null: false
+    t.string "repo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
