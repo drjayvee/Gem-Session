d7bec973aeda722e12b8737d94b71fe7de60ba8e
Jeroen Versteeg <jeroen@alisqi.com>
2025-07-20 15:44:09 +0200

    Add tests for project model

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "prompt muse be present" do
+    project = Project.new
+    project.validate
+
+    refute_empty project.errors[:prompt]
+    assert_match "blank", project.errors[:prompt].first
+
+    project.prompt = "Build this"
+    project.validate
+
+    assert_empty project.errors[:prompt]
+  end
+
+  test "repo_url may be blank" do
+    project = Project.new
+    project.validate
+
+    assert_empty project.errors[:repo_url]
+  end
+
+  test "repo_url must be valid URI" do
+    project = Project.new(repo_url: "example.com")
+    project.validate
+
+    refute_empty project.errors[:repo_url]
+    assert_match "invalid", project.errors[:repo_url].first
+
+    project.repo_url = "http://example.com"
+    project.validate
+
+    assert_empty project.errors[:repo_url]
+  end
 end
