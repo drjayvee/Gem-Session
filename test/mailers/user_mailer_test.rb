410f6694362f18c0f07d4c4b38fb405137ee48d0
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-07-27 13:51:47 +0200

    Fix RuboCop offenses

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,8 +12,8 @@ class UserMailerTest < ActionMailer::TestCase
 
     mail = UserMailer.with(user:).confirm_email
     assert_equal "Confirm your email address to start Gemming", mail.subject
-    assert_equal [user.email_address], mail.to
-    assert_equal ["noodle@gem-session.site"], mail.from
+    assert_equal [ user.email_address ], mail.to
+    assert_equal [ "noodle@gem-session.site" ], mail.from
     assert_match confirm_user_email_url(token: user.confirmation_token), mail.body.encoded
   end
 end
