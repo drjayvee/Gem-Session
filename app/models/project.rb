f12d263b536789e582eff4d6e9ab37ac7ad1606c
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-08-03 14:19:12 +0200

    Style pages, make some elements conditional based on authenticated?

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,5 @@ class Project < ApplicationRecord
 
   validates :prompt, presence: true
   validates :homepage_url, allow_blank: true, format: { with: URI.regexp }
+  # TODO: allow_blank => allow_nil
 end
