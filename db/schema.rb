adfa412f4c6d5de20d7584d3a23446409c796701
Jeroen Versteeg <firm.grip0126@fastmail.com>
2026-05-12 11:09:39 +0200

    Update schema.rb after Rails 8.1 upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_07_070053) do
+ActiveRecord::Schema[8.1].define(version: 2025_09_07_070053) do
   create_table "project_likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "project_id", null: false
     t.datetime "created_at", null: false
+    t.integer "project_id", null: false
     t.datetime "updated_at", null: false
-    t.index [ "project_id" ], name: "index_project_likes_on_project_id"
-    t.index [ "user_id", "project_id" ], name: "index_project_likes_on_user_id_and_project_id", unique: true
-    t.index [ "user_id" ], name: "index_project_likes_on_user_id"
+    t.integer "user_id", null: false
+    t.index ["project_id"], name: "index_project_likes_on_project_id"
+    t.index ["user_id", "project_id"], name: "index_project_likes_on_user_id_and_project_id", unique: true
+    t.index ["user_id"], name: "index_project_likes_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.text "prompt"
-    t.string "homepage_url"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
+    t.string "homepage_url"
+    t.text "prompt"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -36,32 +36,32 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_07_070053) do
   end
 
   create_table "rubygems", force: :cascade do |t|
-    t.string "name", null: false
+    t.datetime "created_at", null: false
     t.text "description", null: false
     t.string "homepage_url", null: false
-    t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email_address", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "project_likes", "projects"
